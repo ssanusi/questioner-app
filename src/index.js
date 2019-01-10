d6ec301d4addb354e3a853343cf2227a6f1f037d
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-10 09:50:37 +0100

    [chore 163054764] correct port no

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use("/api/v1", restRouter);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to REST API for Questioner App" });
 });
-const port = process.env.PORT || 600;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`APi Listerning on Port ${port}`);
 });
