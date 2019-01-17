019fc12cead5dc5cd31f38dbb468f1e56924bc5e
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-01-17 07:42:40 +0100

    update port

--- src/index.js
+++ src/index.js
@@ -10,12 +10,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/api/v1", restRouter);
 
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to REST API for Questioner App" });
 });
 
-app.listen(6000, () => {
-  console.log(`APi Listerning on Port 6000`);
+app.listen(port, () => {
+  console.log(`APi Listerning on Port ${port}`);
 });
 
 export default app;
