a3b147bf538807a34cf89973d44e2078a39e8599
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-23 10:00:16 +0100

    [feature #163203648] implement commentValidator

--- src/api/middleware/commentValidator.js
+++ src/api/middleware/commentValidator.js
@@ -1,11 +1,31 @@
+import validator from "validator";
+import isEmpty from "lodash.isempty";
 
 const validateComment = (req, res, next) => {
-  const { question, userId, comment } = req.body;
-  const error = {}
+  const { questionId, userId, comment } = req.body;
+  const error = {};
 
-  if(!questionId){
-
+  if (!questionId) {
+    error.questionId = "quetionId is Required";
   }
+  if (!userId) {
+    error.userId = "userId is required";
+  }
+  if (!comment) {
+    error.comment = "comment field is required";
+  }
+
+  if (questionId && !validator.isNumeric(parseInt(questionId, 10))) {
+    error.questionId = "questionId should be numeric";
+  }
+  if (userId && !validator.isNumeric(parseInt(userId, 10))) {
+    error.userId = "userId should be numeric";
+  }
+
+  if (isEmpty(error)) {
+    return next();
+  }
+  return res.status(400).json({ error });
 };
 
 export default validateComment;
--- src/api/resourses/comment/CommentController.js
+++ src/api/resourses/comment/CommentController.js
@@ -2,8 +2,11 @@ import Db from "../../../db";
 
 class CommentController {
   static addComment(req, res) {
-
-    Db.query();
+    const queryString = "INSERT INTO comments(questionId,userId,comment) VALUES($1,$2,$3)";
+    const values = [req.body.questionId, req.body.userId, req.body.comment];
+    Db.query(queryString, values)
+      .then()
+      .catch();
   }
 }
 
