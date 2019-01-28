4d36c9799d595ea67f9522ecae224641573931ec
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-28 15:02:52 +0100

    [chore #163520603] added cors middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import logger from "morgan";
+import cors from "cors";
 import bodyParser from "body-parser";
 import restRouter from "./api/restRouter";
 
 const app = express();
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
