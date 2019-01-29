fa207ade034f01943f336b79bc223bb2f5125e3c
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-29 03:45:18 +0100

    [feature #163521315] refactor

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-// import bodyParser from "body-parser";
+import bodyParser from "body-parser";
 import restRouter from "./api/restRouter";
 
 const app = express();
 app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(logger("dev"));
 app.use("/api/v1", restRouter);
