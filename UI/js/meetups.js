f1937e5a6cf8f8aea919388d0d969660c9c59c65
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-30 19:05:29 +0100

    [feature #163587751] refactored meetup

--- UI/js/meetups.js
+++ UI/js/meetups.js
@@ -1,4 +1,11 @@
 window.addEventListener("load", () => {
+  const footerYear = document.getElementById("year");
+  const menuDate = document.getElementById("date");
+  const toggleShow = document.querySelector(".toggle-show");
+  const toggleNavbar = () => {
+    const navContainer = document.getElementsByClassName("nav-container")[0];
+    navContainer.classList.toggle("navbar-toggle-show");
+  };
   const token = JSON.parse(localStorage.getItem("token"));
   const user = JSON.parse(localStorage.getItem("username"));
   if (!token) {
@@ -35,4 +42,13 @@ window.addEventListener("load", () => {
         meetupContainer.innerHTML = output;
       });
     });
+
+  footerYear.innerText = new Date().getFullYear();
+
+  if (menuDate) {
+    menuDate.innerText = new Date().toDateString();
+  }
+  if (toggleShow) {
+    toggleShow.addEventListener("click", toggleNavbar);
+  }
 });
