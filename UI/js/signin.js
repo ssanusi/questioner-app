5564f84874fcc074f8b7fbea19925008d3ca57fe
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-30 10:34:39 +0100

    [feature #163561297] refactor code to helper function on login

--- UI/js/signin.js
+++ UI/js/signin.js
@@ -1,25 +1,10 @@
 const form = document.getElementById("login");
 const statusDiv = document.getElementById("status");
 
-const toJSONString = formhtml => {
-  const obj = {};
-  const elements = formhtml.querySelectorAll("input, select, textarea");
-  for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    const { name, value } = element;
-    if (name) {
-      obj[name] = value.trim();
-    }
-  }
-
-  return JSON.stringify(obj);
-};
-
 const handleFormSubmit = event => {
   event.preventDefault();
 
   const data = toJSONString(form);
-  console.log(data);
   const url = "https://questioner-app-api.herokuapp.com/api/v1/auth/login";
   fetch(url, {
     method: "POST",
