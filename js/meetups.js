e18e85b8f87c2edb0926f21b381db0bf3c503390
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-30 12:23:08 +0100

    [feature #163561530] added username

--- js/meetups.js
+++ js/meetups.js
@@ -1,10 +1,13 @@
 window.addEventListener("load", () => {
   const token = JSON.parse(localStorage.getItem("token"));
+  const user = JSON.parse(localStorage.getItem("username"));
   if (!token) {
     window.location.href = "signin.html";
   }
   const url = "https://questioner-app-api.herokuapp.com/api/v1/meetups";
   const bearer = `Bearer ${token}`;
+  const userLi = document.getElementById("user");
+  userLi.textContent = user;
   fetch(url, {
     method: "GET",
     headers: {
@@ -18,12 +21,16 @@ window.addEventListener("load", () => {
         output += `<div class="card meetup-menu-item">
                       <img src=${element.images[0]} alt="">
                       <p class="meetup">${element.topic}</p>
-                      <p class="meetup"><i class="fas fa-map-marker-alt"></i> ${element.location}</p>
-                      <p class="meetup"><i class="fas fa-calendar-alt"></i> ${moment(element.happeningon).format("MMMM Do YYYY, h:mm:ss a")}</p>
+                      <p class="meetup"><i class="fas fa-map-marker-alt"></i> ${
+                        element.location
+                      }</p>
+                      <p class="meetup"><i class="fas fa-calendar-alt"></i> ${moment(
+                        element.happeningon
+                      ).format("MMMM Do YYYY, h:mm:ss a")}</p>
                       <button class="btn btn-default">
                          <a href="meetup_detail.html">view Questions</a>
                      </button>
-                     </div>`
+                     </div>`;
         const meetupContainer = document.getElementById("meetups");
         meetupContainer.innerHTML = output;
       });
