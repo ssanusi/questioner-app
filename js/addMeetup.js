e359bb293e84228003f83ba5d8714d616f8d40ee
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-31 05:33:31 +0100

    [feature #163587751] implement add meetup

---
+++ js/addMeetup.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("meetup-form");
+const statusDiv = document.getElementById("status");
+
+const handleFormSubmit = event => {
+  event.preventDefault();
+
+  const token = JSON.parse(localStorage.getItem("token"));
+  const user = JSON.parse(localStorage.getItem("username"));
+  if (!token) {
+    window.location.href = "signin.html";
+  }
+
+  const data = toJSONString(form);
+  const formObj = JSON.parse(data);
+  formObj.images = [formObj.images];
+  formObj.tags = [formObj.tags];
+  const url = "https://questioner-app-api.herokuapp.com/api/v1/meetups";
+  const bearer = `Bearer ${token}`;
+  const userLi = document.getElementById("user");
+  userLi.textContent = user;
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(formObj),
+    headers: { Authorization: bearer, "Content-Type": "application/json" }
+  })
+    .then(res => res.json())
+    .then(response => {
+      if (response.status === 201) {
+        statusDiv.innerHTML = `<div class="success"><h4>Meetup created Sucessfully</h4></div>`;
+        setTimeout(() => {
+          window.location.href = "meetups_dashboard.html";
+        }, 2000);
+      }
+      if (response.error) {
+        statusDiv.innerHTML = `<div class="error"><h4>${Object.values(response.error)}</h4></div>`;
+      }
+    });
+};
+
+form.addEventListener("submit", handleFormSubmit);
