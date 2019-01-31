435b116d2ff6f19bec79230a031e45bdea72748a
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-01-31 11:05:50 +0100

    [feature #163587751] refactored admin signin page

--- UI/js/adminSignin.js
+++ UI/js/adminSignin.js
@@ -5,7 +5,7 @@ const handleFormSubmit = event => {
   event.preventDefault();
 
   const data = toJSONString(form);
-  const url = "https://questioner-app-api.herokuapp.com/api/v1/auth/login";
+  const url = "https://questioner-app-api.herokuapp.com/api/v1/admin/auth/login";
   fetch(url, {
     method: "POST",
     body: data,
@@ -13,12 +13,19 @@ const handleFormSubmit = event => {
   })
     .then(res => res.json())
     .then(response => {
+      console.log(response);
       if (response.status === 200) {
         localStorage.setItem("token", JSON.stringify(response.data[0].token));
         localStorage.setItem("username", JSON.stringify(response.data[0].user.username));
         statusDiv.innerHTML = `<div class="success"><h4>${response.data[0].message}</h4></div>`;
         setTimeout(() => {
           window.location.href = "meetups_dashboard.html";
+        }, 2000);
+      }
+      if (response.status === 401) {
+        statusDiv.innerHTML = `<div class="error"><h4>Unauthorized</h4></div>`;
+        setTimeout(() => {
+          window.location.href = "signin.html";
         }, 2000);
       }
       if (response.error) {
