f33a047c5c19420fd20c9ca9f9d6c111169af370
sulaiman sanusi <sulaiman.sanusi@icloud.com>
2019-02-07 11:14:00 +0100

    [chores #163779220] refactor cors

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import restRouter from "./api/restRouter";
 
 const app = express();
 app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
