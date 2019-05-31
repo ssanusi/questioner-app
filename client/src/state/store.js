d99d379be04069cb6faa232acbaa7530c2806833
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-05-31 01:45:02 +0100

    feature(landingPage) Implement Landing page
    -add logo
    -add header
    
    [delivers #166361062]

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -4,6 +4,9 @@ import {
 } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { meetup } from './meetup';
 
 // eslint-disable-next-line max-len
@@ -12,14 +15,25 @@ const rootReducer = combineReducers({
   [meetup.stateKey]: meetup.reducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
 const reduxDevTools = process.env.NODE_ENV === 'production'
   ? p => p
   : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = createStore(
-  rootReducer,
+const pReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(
+  pReducer,
   {},
-  compose(applyMiddleware(...middleware), reduxDevTools),
+  compose(
+    applyMiddleware(...middleware),
+    reduxDevTools,
+  ),
 );
 
-export default store;
+export const persister = persistStore(store);
